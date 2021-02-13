b4ca92730c0cef2b2fe3a6ce7a98790c7415662e
ceciliallee <chaelee@email.unc.edu>
2021-02-13 12:53:08 -0500

    bunch of stuffs

---
+++ front/js/index.js
@@ -0,0 +1,26 @@
+/* Navbar */
+
+// get the hamburger element
+var hamburger = document.getElementById("hamburger");
+
+// get the menu element
+var menu = document.getElementById("menu");
+
+// get the overlay element
+var overlay = document.getElementById("overlay");
+
+// define toggle function
+function toggleMenu() {
+    // Either adds/removes active class from element
+    menu.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+// execute toggle function from hamburger on click
+hamburger.addEventListener("click", toggleMenu);
+
+// exuecute toggle function from menu on click
+menu.addEventListener("click", toggleMenu);
+
+// exuecute toggle function from overlay on click
+overlay.addEventListener("click", toggleMenu);
