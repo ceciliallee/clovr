469cd82b7784ed43a8355c6228866b4213d88987
Amin Zamani <abz@email.unc.edu>
2021-03-02 01:31:31 -0500

    Added email verification

--- app.py
+++ app.py
@@ -5,10 +5,19 @@ import firebase_admin
 import string
 import random
 import re
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.secret_key = "asdfasfdasfdsafasddfsadfasdfsadfdas"
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = os.environ['EMAIL']
+app.config['MAIL_PASSWORD'] = os.environ['EMAIL_PASS']
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
 base = os.getcwd()
 f_credential = json.loads(os.environ['GOOGLE_APPLICATION_CREDS'])
 f_credentials = firebase_admin.credentials.Certificate(f_credential)
@@ -28,8 +37,11 @@ list_items = ["csinterest[]", "hobbies[]"]
 from firebase_admin import db as database
 
 current_db_items = {}
+current_db_emails = {}
 for i in database.reference('/users').get().keys():
-    current_db_items[i] = 1
+    email_tmp = database.reference('/users/'+i).get()['email']
+    current_db_items[i] = email_tmp
+    current_db_emails[email_tmp] = i
 
 @app.route('/')
 def index():
@@ -64,6 +76,13 @@ def submit():
             return 'success?'
         else:
             return message
+@app.route('/verify/<unique_key>')
+def verify(unique_key):
+    if unique_key in current_db_items:
+        database.reference('/users/'+unique_key).update({"emailVerified": "true"})
+        return "Successfully verified email! You can now safely close this page."
+    else:
+        return "No user found to verify with that key. Please make sure your key is correct."
 #Takes in a dictionary for vals
 def uploadSurveyContent(vals):
     user_key = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(10))
@@ -72,11 +91,16 @@ def uploadSurveyContent(vals):
         user_key = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(10))
     path = '/users/' + user_key
     ref_path = database.reference(path)
-    current_db_items[user_key] = 1
+    current_db_items[user_key] = vals['email']
+    current_db_emails[vals['email']] = user_key
     for i in vals:
         value = vals[i]
         key = i
         ref_path.update({key : value})
+    ref_path.update({"emailVerified": "false"})
+    msg = Message("Welcome to Clovr!", sender=os.environ["EMAIL"], recipients=[vals['email']])
+    msg.body = "Hello " + vals['first'] + " " + vals['last'] + ",\n\tWe're excited that you want to participate in this digital social event. Please verify your email using the following link below:\nhttp://clovru.herokuapp.com/verify/"+user_key + "\n\n\tSee you on March 17th at 7:00pm EST!\nBest,\n  the Clovr team at UNC-Chapel Hill."
+    mail.send(msg)
 
 def validateForm():
     for i in form.keys():
@@ -107,6 +131,8 @@ def validateForm():
 
     if not re.search(regex_email, request.form['email']):
         return -1, "Invalid email format"
+    if request.form['email'] in current_db_emails:
+        return -1, "That email has already been used in submitting a form."
     return 0, "Valid form"
 
 
